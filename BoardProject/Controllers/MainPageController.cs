053927b7335b3607cd00352f88ad62050a8dedea
Roiamz <57364615+Roiamz@users.noreply.github.com>
2019-12-07 21:41:20 +0200

    MainPage
    added a MainPage

---
+++ BoardProject/Controllers/MainPageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BoardProject.Models;
+using BoardProject.Data;
+
+namespace BoardProject.Controllers
+{
+    public class MainPageController : Controller
+    {
+        // GET Home/MainPage/
+
+        public IActionResult MainPage()
+        {
+            return View();
+        }
+    }
+}
