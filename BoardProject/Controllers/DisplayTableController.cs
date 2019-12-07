5575f7e811bb3cee802fe4a8af264b0855eb6c7c
Shaul1Kr <shaulikr07@gmail.com>
2019-12-07 20:39:08 +0200

    DisplayTable
    Added DisplayTable to the project

---
+++ BoardProject/Controllers/DisplayTableController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BoardProject.Models;
+using BoardProject.Data;
+
+namespace BoardProject.Controllers
+{
+    public class DisplayTableController : Controller
+    {
+        // GET Views/DisplayTable/
+
+        public IActionResult DisplayTable()
+        {
+            return View();
+        }
+    }
+}
