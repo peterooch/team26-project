f768b976a94a8deefd0204387a6a1e2382845e44
Baruch Rutman <peterooch@gmail.com>
2019-12-09 21:25:36 +0200

    Add Controller Method Tests

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,14 +1,78 @@
 using System;
 using Xunit;
+using BoardProject.Models;
+using BoardProject.Data;
+using BoardProject.Controllers;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Tests
 {
-    public class UnitTest1
+    public class TestControllerMethods
     {
         [Fact]
-        public void DummyTest()
+        public void TestHomeController_Index()
         {
-            Assert.True(1 + 2 == 3);
+            HomeController controller = new HomeController();
+            ViewResult result = controller.Index() as ViewResult;
+            Assert.NotNull(result);
+        }
+        [Fact]
+        public void TestHomeController_Debug()
+        {
+            /* FIXME: Fix issues with DataContext not finding the tables */
+            HomeController controller = new HomeController();
+            ViewResult result = controller.Debug() as ViewResult;
+            Assert.NotNull(result);
+        }
+        [Fact]
+        public void TestHomeController_Contact()
+        {
+            HomeController controller = new HomeController();
+            ViewResult result = controller.Contact() as ViewResult;
+            Assert.NotNull(result);
+        }
+        [Fact]
+        public void TestBoardViewController_Index()
+        {
+            BoardViewController controller = new BoardViewController();
+            ViewResult result = controller.Index(null) as ViewResult;
+            Assert.NotNull(result);
+        }
+        [Fact]
+        public void TestLoginPageController_LoginPage()
+        {
+            LoginPageController controller = new LoginPageController();
+            ViewResult result = controller.LoginPage() as ViewResult;
+            Assert.NotNull(result);
+        }
+        [Fact]
+        public void TestMainPageController_MainPage()
+        {
+            MainPageController controller = new MainPageController();
+            ViewResult result = controller.MainPage() as ViewResult;
+            Assert.NotNull(result);
+        }
+        [Fact]
+        public void TestMIController_MI()
+        {
+            MIController controller = new MIController();
+            ViewResult result = controller.MI() as ViewResult;
+            Assert.NotNull(result);
+        }
+        [Fact]
+        public void TestTileController_AddTile()
+        {
+            TileController controller = new TileController();
+            ViewResult result = controller.AddTile() as ViewResult;
+            Assert.NotNull(result);
+        }
+        [Fact]
+        public void TestDisplayTableController_DisplayTable()
+        {
+            DisplayTableController controller = new DisplayTableController();
+            ViewResult result = controller.DisplayTable() as ViewResult;
+            Assert.NotNull(result);
         }
     }
 }
