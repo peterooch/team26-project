a9c826a22b72aa792ca160bc0321eac49df30672
Roiamz <57364615+Roiamz@users.noreply.github.com>
2019-12-12 17:00:39 +0200

    Add HTML skeleton

---
+++ BoardProject/Controllers/UserPrefController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BoardProject.Controllers
+{
+    public class UserPrefController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
