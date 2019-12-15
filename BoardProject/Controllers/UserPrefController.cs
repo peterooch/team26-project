509c8c9334c485d000dbea0f8a9e8c937554e80d
Roiamz <57364615+Roiamz@users.noreply.github.com>
2019-12-15 14:31:27 +0200

    User pref

--- BoardProject/Controllers/UserPrefController.cs
+++ BoardProject/Controllers/UserPrefController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using BoardProject.Data;
+using BoardProject.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace BoardProject.Controllers
 {
@@ -12,7 +14,18 @@ namespace BoardProject.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            int UserID = HttpContext.Session.GetInt32("SelectedUser") ?? default;
+            using var DBCon = new DataContext();
+            User UserObject;
+            if (UserID == default)
+                return View(null);
+
+            UserObject = new User(DBCon.UserData.Find(UserID));
+
+            if (UserObject == null)
+                return View(null);
+            
+            return View(UserObject);
         }
     }
 }
