39e95d139b5da6031ff95bf85809970473472c0b
Shaul1Kr <shaulikr07@gmail.com>
2019-12-17 15:33:31 +0200

    Add login error model
    To be used in login page

---
+++ BoardProject/Models/Errors.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoardProject.Models
+{
+    public class LoginError
+    {
+        public string username { get; set; }
+        public bool username_error { get; set; }
+        public bool password_error { get; set; }
+    }
+}
