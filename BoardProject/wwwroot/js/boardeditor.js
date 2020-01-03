e3704dac323b9822761f0cc7c46ba6a4cfed901a
Baruch Rutman <peterooch@gmail.com>
2020-01-03 10:57:23 +0200

    Migrate JavaScript code into separate file
    Remove hack for custom scripts

---
+++ BoardProject/wwwroot/js/boardeditor.js
@@ -0,0 +1,35 @@
+﻿function AddTile(tileid, bg_color, tile_text, image_src, image_name) {
+    var tile_container = document.querySelector(".tile_container");
+    var outerdiv = document.createElement("DIV");
+    outerdiv.style.padding = padding;
+    var innerdiv = document.createElement("DIV");
+    innerdiv.className = "tiles";
+    innerdiv.id = tileid;
+    innerdiv.style.backgroundColor = "#" + bg_color;
+    var image_text = document.createTextNode(tile_text);
+    innerdiv.appendChild(image_text);
+    var edit = document.createElement("IMG");
+    edit.src = "/images/edit.png";
+    edit.onclick = function (e) {
+        EditTile(e.target.parentElement.id);
+    };;
+    innerdiv.appendChild(edit);
+    var del = document.createElement("IMG");
+    del.src = "/images/delete.png";
+    del.onclick = function (e) {
+        RemoveTile(e.target.parentElement.id);
+    };
+    innerdiv.appendChild(del);
+    innerdiv.appendChild(document.createElement("BR"));
+    var img = document.createElement("IMG");
+    img.src = image_src;
+    img.alt = image_name;
+    img.style.width = "15vw";
+    img.style.height = "26.25vh";
+    innerdiv.appendChild(img);
+    outerdiv.appendChild(innerdiv);
+    tile_container.appendChild(outerdiv);
+}
+function RemoveTile(tileid) {
+    document.getElementById(tileid).parentElement.remove();
+}
