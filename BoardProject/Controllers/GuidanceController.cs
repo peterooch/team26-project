58876c4f3385d174bcbef3000c4bc9d15bddf847
MatanBarazi <moshem431@gmail.com>
2020-01-13 03:13:17 +0200

    Add Guidance page and connection to main page
    Add Guidance page and connection to main page , I add css with style to title.

---
+++ BoardProject/Controllers/GuidanceController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoardProject.Controllers
+{
+    public class GuidanceController : Controller
+    {
+        public IActionResult Guidance()
+        {
+            return View();
+        }
+    }
+}
